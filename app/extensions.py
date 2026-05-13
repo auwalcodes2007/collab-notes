ae562afb23ba52a64021fd11b770603467389dba
Auwal <mohammedauwalhassan07@gmail.com>
2026-05-13 21:00:04 +0100

    chore: create extensions

---
+++ app/extensions.py
@@ -0,0 +1,12 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from sqlalchemy.orm import DeclarativeBase
+from flask_migrate import Migrate
+from flask_bcrypt import Bcrypt
+
+class Base(DeclarativeBase):
+    pass
+db = SQLAlchemy(model_class=Base)
+login_manager = LoginManager()
+bcrypt = Bcrypt()
+migrate = Migrate()
